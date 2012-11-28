e67c60b7a03cfd3bc3c38309c4eb61ed3c8b5ee8
IvanovEAn <clydezero@aol.ru>
2012-11-28 05:04:05 +0400

    My version of project

---
+++ ExchangeClient/ExchangeClient/ContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContentViewController.h
+//  ExchangeClient
+//
+//  Created by Администратор on 11.11.12.
+//  Copyright (c) 2012 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentViewController : UIViewController
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil message:(NSDictionary*)message;
+@end
---
+++ ExchangeClient/ExchangeClient/XMLHandle.c
@@ -0,0 +1,9 @@
+//
+//  XMLHandle.c
+//  ExchangeClient
+//
+//  Created by LSA on 20/11/2012.
+//  Copyright (c) 2012 Администратор. All rights reserved.
+//
+
+#include <stdio.h>
