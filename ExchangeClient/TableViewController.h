96feaf3407386887a24ae28a98325f3ced29a2bc
IvanovEAn <clydezero@aol.ru>
2012-11-28 05:22:12 +0400

    My version 2

---
+++ ExchangeClient/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  ExchangeClient
+//
+//  Created by Администратор on 11.11.12.
+//  Copyright (c) 2012 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
